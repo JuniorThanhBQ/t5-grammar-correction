a13db08e5896e64f37571bad0aad717bf5c06256
JuniorThanhBQ <2351050164thanh@ou.edu.vn>
2025-07-13 19:41:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st_model_load = st.text('Đang nạp dữ liệu')
 @st.cache_resource
 def load_model():
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    gec = pipeline("text2text-generation", model="JuniorThanh/t5_CoEdit_Model", tokenizer=tokenizer, device=0)       # ép chạy trên CPU)
+    gec = pipeline("text2text-generation", model="JuniorThanh/t5_CoEdit_Model", tokenizer=tokenizer)       # ép chạy trên CPU)
     return tokenizer, gec
 
 tokenizer, gec = load_model()
