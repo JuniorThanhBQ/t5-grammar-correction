95fc509e6f5d7ec021830e65d7e38228aa483239
JuniorThanhBQ <2351050164thanh@ou.edu.vn>
2025-07-13 14:15:45 +0700

    Improve user GUI

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import streamlit as st
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import torch
 
+
 model_name = "JuniorThanh/t5-base-medium-grammar-correction-ou"
 max_input_length = 512
 
-st.set_page_config(layout="centered")
-st.header("Grammar Correction")
+st.set_page_config(page_title="NLPerfect", layout="wide")
+st_model_load = st.text('Đang nạp dữ liệu')
 
-st_model_load = st.text('Loading model...')
 
 @st.cache_resource
 def load_model():
@@ -17,41 +17,85 @@ def load_model():
     return tokenizer, model
 
 tokenizer, model = load_model()
-st.success('Model loaded!')
+st.success('Khởi tạo chương trình thành công!')
 st_model_load.text("")
 
 # Input section
 if "corrected_text" not in st.session_state:
     st.session_state.corrected_text = ""
 
-# Nhập liệu trực tiếp
-text_input = st.text_area('Text to correct grammar for', height=300)
 
-def generate_correction(input_text):
-    # Tokenize input
-    inputs = ["fix grammaticality in this sentence: " + input_text + "."]
-    inputs = tokenizer(inputs, return_tensors="pt", truncation=True, max_length=max_input_length)
+# Header
+st.markdown("<h2 style='text-align:left'>Trường Đại học Mở TP.Hồ Chí Minh</h2>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align:left'>Giới thiệu chung</h3>", unsafe_allow_html=True)
+st.write("""
+Công cụ hỗ trợ kiểm tra ngữ pháp trong văn bản Tiếng Anh do nhóm NLPerfect thiết kế dựa trên huấn luyện từ hai mô hình CoEdit và T5, có khả năng phân tích những lỗi sai ngữ pháp từ cơ bản đến nâng cao. Đặc biệt, người dùng có thể sử dụng công cụ mà không lo bị giới hạn số lần dùng.
+""")
 
-    outputs = model.generate(
-    **inputs,
-    num_beams=5,             
-    do_sample=False,          
-    repetition_penalty=2.0,   
-    no_repeat_ngram_size=3,  
-    length_penalty=1.0,       
-    max_length=inputs["input_ids"].size(1) + 5,  
-    early_stopping=True
-)
+st.markdown("<h3 style='text-align:left'>Hướng dẫn sử dụng</h3>", unsafe_allow_html=True)
+html_instructions = """
+<ol style="text-align:left; margin-left:1em;">
+  <li>Nhập văn bản tiếng Anh trong cột "Văn Bản Đầu Vào" (Lưu ý: không nhập chữ tiếng Việt có dấu)</li>
+  <li>Bấm kiểm tra ngữ pháp sau khi hoàn tất nhập liệu.</li>
+  <li>Xem kết quả ở cột kết quả.</li>
+</ol>
+"""
+st.markdown(html_instructions, unsafe_allow_html=True)
 
-    decoded_outputs = tokenizer.batch_decode(outputs, skip_special_tokens=True)
-    st.session_state.corrected_text = decoded_outputs[0].strip()
+# Hai cột cho input và output
+col1, col2 = st.columns([3, 2])
 
-# Button
-if st.button('Fix grammar'):
-    if text_input.strip():  # tránh input rỗng
-        generate_correction(text_input)
+with col1:
+    st.markdown("### Văn Bản Đầu Vào")
+    text_input = st.text_area("Hãy nhập văn bản vào khung bên dưới.", height=300)
 
-# Display output
-if st.session_state.corrected_text:
-    st.subheader("Corrected Sentence")
-    st.markdown(f"✅ **{st.session_state.corrected_text}**")
+
+if text_input.isascii():
+    def generate_correction(input_text):
+        # Tokenize input
+        inputs = ["fix grammaticality in this sentence: " + input_text]
+        inputs = tokenizer(inputs, return_tensors="pt", truncation=True, max_length=max_input_length)
+
+        outputs = model.generate(
+        **inputs,
+        num_beams=5,              # sử dụng beam search để tạo phản hồi chất lượng ổn định
+        do_sample=False,          # không dùng sampling (loại bỏ ngẫu nhiên)
+        repetition_penalty=2.0,   # tránh lặp từ
+        no_repeat_ngram_size=3,   # không lặp cụm từ 3-gram
+        length_penalty=1.0,       # độ dài phản hồi tương đương đầu vào
+        max_length=inputs["input_ids"].size(1) + 5,  # chỉ dài hơn đầu vào tối đa 5 token
+        early_stopping=True
+    )
+        decoded_outputs = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+        st.session_state.corrected_text = decoded_outputs[0].strip()
+
+
+with col2:
+    st.markdown("### Kết Quả")
+    if st.session_state.get("result"):
+        st.markdown(st.session_state["result"])
+    else:
+        st.markdown("_Kết quả kiểm tra sẽ hiển thị ở đây._")
+
+# Nút "Kiểm tra ngữ pháp"
+if st.button("Kiểm tra ngữ pháp"):
+    if text_input.isascii():
+        if text_input.strip():  # tránh input rỗng
+            generate_correction(text_input)
+
+        with col2:
+            if st.session_state.corrected_text:
+                st.markdown(f"**{st.session_state.corrected_text}**")
+    else:
+        with col2:
+            if st.session_state.corrected_text:
+                st.markdown("Xuất hiện văn bản không hợp lệ trong input!")
+        
+st.markdown("<h3 style='text-align:left;margin-top:5%'>Khảo sát sau khi sử dụng</h3>", unsafe_allow_html=True)
+st.write("""
+Cảm ơn mọi người đã dành thời gian sử dụng công cụ của nhóm mình. Nếu mọi người có điều cần góp ý thì bấm vào nút bên dưới để thực hiện khảo sát nhé, nhóm mình xin chân thành cảm ơn.
+""")
+st.link_button("Khảo sát và phản hồi", "#", type="primary")
+
+st.markdown("---")
+st.markdown("<div style='text-align:center;font-weight:700'>Công cụ hỗ trợ kiểm tra ngữ pháp Tiếng Anh của nhóm NLPerfect.</div>", unsafe_allow_html=True)
